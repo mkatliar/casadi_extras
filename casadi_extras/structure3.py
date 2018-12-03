39fda0116e1b97f18f68340f8b30537061cabccf
Mikhail Katliar <mikhail.katliar@tuebingen.mpg.de>
2018-12-03 16:20:46 +0100

    Added LICENSE file and license headers.

--- casadi_extras/structure3.py
+++ casadi_extras/structure3.py
@@ -1,26 +1,25 @@
-#
-#     This file is part of CasADi.
-#
-#     CasADi -- A symbolic framework for dynamic optimization.
-#     Copyright (C) 2010-2014 Joel Andersson, Joris Gillis, Moritz Diehl,
-#                             K.U. Leuven. All rights reserved.
-#     Copyright (C) 2011-2014 Greg Horn
-#
-#     CasADi is free software; you can redistribute it and/or
-#     modify it under the terms of the GNU Lesser General Public
-#     License as published by the Free Software Foundation; either
-#     version 3 of the License, or (at your option) any later version.
-#
-#     CasADi is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#     Lesser General Public License for more details.
-#
-#     You should have received a copy of the GNU Lesser General Public
-#     License along with CasADi; if not, write to the Free Software
-#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-#
-#
+##
+## Copyright (c) 2018 Mikhail Katliar.
+## 
+## This file is part of CasADi Extras 
+## (see https://github.com/mkatliar/casadi_extras).
+##
+## This file is a derived work based on structure3.py file from the CasADi framework:
+## https://github.com/casadi/casadi/blob/master/swig/python/tools/structure3.py
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+## 
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 from casadi import *
 
 
